e36ff03564cd4c6424efa59c7eca3dcbbad56b2d
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-06-10 14:32:55 +0530

    first working version with multithreaded option

--- download_shorts_multithreaded.py
+++ download_shorts_multithreaded.py
@@ -1,6 +1,8 @@
+#let's try multithreading
 import yt_dlp
 import os
 import subprocess
+import concurrent.futures
 
 def get_short_links(channel_url):
     ydl_opts = {
@@ -8,6 +10,7 @@ def get_short_links(channel_url):
         'extract_flat': True,
         'playlist_end': 100,
     }
+    
     # try to parse the channel URL by yourself -- works for the @username, /channel and /c cases
     if '/@' in channel_url:
         channel_username = channel_url.split('/@')[1].split('/')[0]
@@ -25,23 +28,32 @@ def get_short_links(channel_url):
             print("No videos found on the channel.")
             return []
 
+def download_video(link, output_path, format):
+    link = link.strip()
+    try:
+        if format == 'webm':
+            subprocess.run(['yt-dlp', '--quiet', '--format', 'bestvideo[ext=webm]+bestaudio[ext=webm]/best[ext=webm]', '--output', os.path.join(output_path, '%(title)s.%(ext)s'), link], check=True)
+        else:
+            subprocess.run(['yt-dlp', '--quiet', '--format', format , '--output', os.path.join(output_path, '%(title)s.%(ext)s'), link], check=True)
+        
+        print(f"Downloaded: {link}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error downloading {link}: {e}")
+
 def download_videos_from_links(links, output_path, format):
-    total_links = len(links)
-    
-    for index, link in enumerate(links, start=1):
-        link = link.strip()
-        try:
-            subprocess.run(['yt-dlp', '--quiet', '--format',format, '--output', os.path.join(output_path, '%(title)s.%(ext)s'), link], check=True)
-            print(f"Downloading video {index}/{total_links}: {link} - Downloaded successfully")
-        except subprocess.CalledProcessError as e:
-            print(f"Error downloading {link}: {e}")
+    # max_workers can be adjusted based on your system's capabilities
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(download_video, link, output_path, format) for link in links]
+        concurrent.futures.wait(futures)
 
 if __name__ == '__main__':
     channel_url = 'https://www.youtube.com/@Techlism'  # Replace with your desired channel URL / This channel (mine) doesn't have any shorts
     output_directory = 'downloads'
     os.makedirs(output_directory, exist_ok=True)
-    format = 'mp4' # best quality is available with webm but it is not supported by all video players
+
     short_links = get_short_links(channel_url)
+    format = 'webm' # best quality is available with webm but it is not supported by all video players
+    
     if short_links:
         download_videos_from_links(short_links, output_directory, format)
     else:
